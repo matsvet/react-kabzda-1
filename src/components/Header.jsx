dbe1e31d076081bba171f7b551712b83e83dddb3
Matvey <mat.svetozaroff@gmail.com>
2022-02-11 20:19:47 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src='https://sun9-50.userapi.com/sun9-22/impg/5rto0BCni8VW4LwgF7eZEdq9wZe19k8807uIXA/DuTrcI3rsmI.jpg?size=750x741&quality=96&proxy=1&sign=09e03727ea4864cb1566648f1899bfd1&type=album' />
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://lh3.googleusercontent.com/pw/AM-JKLWC7NkvXkrctpf19ahiGMaU9beTgC7rSzQ4nzzD7WaSqSQTtRBDBDVnn8UjaU-ElqG0-6TSU5HrUCB4Qs8oE4gPdkHweVCW6mmPjKSYJxCUyXjc0t-uVeO99Td3MBTNLgFME4b5m1s2ZPqgB01UM6heBw=w958-h220-no?authuser=0'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                MY POSTS
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+                <div>
+                    post 3
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
